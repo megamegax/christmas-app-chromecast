770875bf88a75870170ab0e47a30feaf345d2b0c
Hunyady Mihály <mihaly.hunyady@emarsys.com>
2022-12-23 13:55:37 +0100

    add support for iOS

--- android/app/src/main/kotlin/com/example/christmas_app_chromecast/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/christmas_app_chromecast/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.christmas_app_chromecast
 
-import io.flutter.embedding.android.FlutterActivity
+import com.google.android.gms.cast.framework.CastContext
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        CastContext.getSharedInstance(applicationContext)
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,246 +1,172 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_chromecast_example/player/cast_mini_media_controls.dart';
-import 'package:flutter_chromecast_example/service_discovery.dart';
-import 'package:dart_chromecast/casting/cast.dart';
-import 'package:flutter_chromecast_example/device_picker.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_cast_video/flutter_cast_video.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Karácsony',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Karácsony App'),
-    );
+    return MaterialApp(home: CastSample());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+class CastSample extends StatefulWidget {
+  static const _iconSize = 50.0;
+  static const csengo =
+      "https://raw.githubusercontent.com/megamegax/christmas-app/master/app/src/main/res/raw/csengo.mp3";
+  static const menybol =
+      "https://raw.githubusercontent.com/megamegax/christmas-app/master/app/src/main/res/raw/mennybol.mp3";
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _CastSampleState createState() => _CastSampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  bool _servicesFound = false;
-  bool _castConnected = false;
-  ServiceDiscovery _serviceDiscovery;
-  CastSender _castSender;
+class _CastSampleState extends State<CastSample> {
+  ChromeCastController _controller;
+  AppState _state = AppState.idle;
+  bool _playing = false;
 
-  List _videoItems = [
-    CastMedia(
-      title: 'Zene',
-      contentId:
-          'https://raw.githubusercontent.com/megamegax/christmas-app/master/app/src/main/res/raw/mennybol.mp3',
-      images: [
-        'https://img.casual.hu/shops/2013/images/item/szalveta-karacsonyi-angyal-es-nyuszi-xmas-angel-and-rabbitts-581588.jpg?v=1607969862'
-      ],
-    ),
-    CastMedia(
-        title: 'Csengő',
-        contentId:
-            'https://raw.githubusercontent.com/megamegax/christmas-app/master/app/src/main/res/raw/csengo.mp3',
-        images: [
-          'https://images.minimano.hu/bolcsi_ovi_felkeszules/ovisjelek/csengo/ovodai_jelkeszlet_16_darabos_csengo_1.jpg'
-        ])
-  ];
-
+  var currentMp3 = CastSample.csengo;
+  @override
   void initState() {
     super.initState();
 
-    _reconnectOrDiscover();
-  }
-
-  _reconnectOrDiscover() async {
-    bool reconnectSuccess = await reconnect();
-    if (!reconnectSuccess) {
-      _discover();
-    }
-  }
-
-  _discover() async {
-    _serviceDiscovery = ServiceDiscovery();
-    _serviceDiscovery.changes.listen((_) {
-      setState(
-          () => _servicesFound = _serviceDiscovery.foundServices.length > 0);
-    });
-    _serviceDiscovery.startDiscovery();
-  }
-
-  Future<bool> reconnect() async {
-    final prefs = await SharedPreferences.getInstance();
-    String host = prefs.getString('cast_session_host');
-    String name = prefs.getString('cast_session_device_name');
-    String type = prefs.getString('cast_session_device_type');
-    String sourceId = prefs.getString('cast_session_sender_id');
-    String destinationId = prefs.getString('cast_session_destination_id');
-    if (null == host ||
-        null == name ||
-        null == type ||
-        null == sourceId ||
-        null == destinationId) {
-      return false;
-    }
-    CastDevice device = CastDevice(
-        name: name,
-        host: host,
-        port: prefs.getInt('cast_session_port') ?? 8009,
-        type: type);
-    _castSender = CastSender(device);
-    StreamSubscription subscription = _castSender.castSessionController.stream
-        .listen((CastSession castSession) {
-      print('CastSession update ${castSession.isConnected.toString()}');
-      if (castSession.isConnected) {
-        _castSessionIsConnected(castSession);
-      }
-    });
-    bool didReconnect = await _castSender.reconnect(
-      sourceId: sourceId,
-      destinationId: destinationId,
-    );
-    if (!didReconnect) {
-      subscription.cancel();
-      _castSender = null;
-    }
-    return didReconnect;
-  }
-
-  void disconnect() async {
-    if (null != _castSender) {
-      await _castSender.disconnect();
-      final prefs = await SharedPreferences.getInstance();
-      prefs.remove('cast_session_host');
-      prefs.remove('cast_session_port');
-      prefs.remove('cast_session_device_name');
-      prefs.remove('cast_session_device_type');
-      prefs.remove('cast_session_sender_id');
-      prefs.remove('cast_session_destination_id');
-      setState(() {
-        _castSender = null;
-        _servicesFound = false;
-        _castConnected = false;
-        _discover();
-      });
-    }
-  }
-
-  void _castSessionIsConnected(CastSession castSession) async {
-    setState(() {
-      _castConnected = true;
-    });
-
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setString('cast_session_host', _castSender.device.host);
-    prefs.setInt('cast_session_port', _castSender.device.port);
-    prefs.setString('cast_session_device_name', _castSender.device.name);
-    prefs.setString('cast_session_device_type', _castSender.device.type);
-    prefs.setString('cast_session_sender_id', castSession.sourceId);
-    prefs.setString('cast_session_destination_id', castSession.destinationId);
-  }
-
-  void _connectToDevice(CastDevice device) async {
-    // stop discovery, only has to be on when we're not casting already
-    _serviceDiscovery.stopDiscovery();
-
-    _castSender = CastSender(device);
-    StreamSubscription subscription = _castSender.castSessionController.stream
-        .listen((CastSession castSession) {
-      if (castSession.isConnected) {
-        _castSessionIsConnected(castSession);
-      }
-    });
-    bool connected = await _castSender.connect();
-    if (!connected) {
-      // show error message...
-      subscription.cancel();
-      _castSender = null;
-      return;
-    }
-
-    // SAVE STATE SO WE CAN TRY TO RECONNECT!
-    _castSender.launch();
-  }
-
-  Widget _buildVideoListItem(BuildContext context, int index) {
-    CastMedia castMedia = _videoItems[index];
-    return GestureDetector(
-      onTap: () => null != _castSender ? _castSender.load(castMedia) : null,
-      child: Card(
-        child: Column(
-          children: <Widget>[
-            Container(
-                width: 200,
-                height: 200,
-                child: Image.network(castMedia.images.first)),
-            Text(castMedia.title),
-          ],
-        ),
-      ),
-    );
+    currentMp3 = CastSample.csengo;
   }
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> actionButtons = [];
-    if (_servicesFound || _castConnected) {
-      IconData iconData = _castConnected ? Icons.cast_connected : Icons.cast;
-      actionButtons.add(
-        IconButton(
-          icon: Icon(iconData),
-          onPressed: () {
-            if (_castConnected) {
-              print('SHOW DISCONNECT DIALOG!');
-              // for now just immediately disconnect
-              disconnect();
-              return;
-            }
-            Navigator.of(context).push(new MaterialPageRoute(
-              builder: (BuildContext context) => DevicePicker(
-                  serviceDiscovery: _serviceDiscovery,
-                  onDevicePicked: _connectToDevice),
-              fullscreenDialog: true,
-            ));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Plugin example app'),
+        actions: <Widget>[
+          ChromeCastButton(
+            size: CastSample._iconSize,
+            color: Colors.white,
+            onButtonCreated: _onButtonCreated,
+            onSessionStarted: _onSessionStarted,
+            onSessionEnded: () => setState(() => _state = AppState.idle),
+            onRequestCompleted: _onRequestCompleted,
+            onRequestFailed: _onRequestFailed,
+          ),
+        ],
+      ),
+      body: Center(child: _handleState()),
+    );
+  }
+
+  Widget _handleState() {
+    switch (_state) {
+      case AppState.idle:
+        return Text('ChromeCast not connected');
+      case AppState.connected:
+        return Text('No media loaded');
+      case AppState.mediaLoaded:
+        return _mediaControls();
+      case AppState.error:
+        return Text('An error has occurred');
+      default:
+        return Container();
+    }
+  }
+
+  Widget _mediaControls() {
+    return Column(
+      children: [
+        RadioListTile(
+          title: Text("Csengő"),
+          value: CastSample.csengo,
+          groupValue: currentMp3,
+          onChanged: (value) async {
+            _controller.stop();
+            await _controller.loadMedia(CastSample.csengo);
+            setState(() {
+              currentMp3 = CastSample.csengo;
+            });
+            _playPauseMenybol();
           },
         ),
-      );
-    }
-    // TODO: if connected, also show (even if there are no services found)
-
-    List<Widget> stackChildren = [
-      ListView.builder(
-        itemBuilder: _buildVideoListItem,
-        itemCount: _videoItems.length,
-      ),
-    ];
-
-    if (null != _castSender) {
-      stackChildren.add(Positioned(
-        bottom: 0.0,
-        right: 0.0,
-        left: 0.0,
-        child: Card(child: CastMiniMediaControls(_castSender, canExtend: true)),
-      ));
-    }
-
-    return Builder(builder: (BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-          actions: actionButtons,
+        RadioListTile(
+          title: Text("Menyből az angyal"),
+          value: CastSample.menybol,
+          groupValue: currentMp3,
+          onChanged: (value) async {
+            _controller.stop();
+            await _controller.loadMedia(CastSample.menybol);
+            setState(() {
+              currentMp3 = CastSample.menybol;
+            });
+            _playPauseMenybol();
+          },
         ),
-        body: Stack(children: stackChildren),
-      );
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _RoundIconButton(
+              icon: Icons.replay_10,
+              onPressed: () =>
+                  _controller.seek(relative: true, interval: -10.0),
+            ),
+            _RoundIconButton(
+                icon: _playing ? Icons.pause : Icons.play_arrow,
+                onPressed: _playPauseMenybol),
+            _RoundIconButton(
+              icon: Icons.forward_10,
+              onPressed: () => _controller.seek(relative: true, interval: 10.0),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
+  Future<void> _playPauseMenybol() async {
+    final playing = await _controller.isPlaying();
+    if (playing) {
+      await _controller.pause();
+    } else {
+      await _controller.play();
+    }
+    setState(() => _playing = !playing);
+  }
+
+  Future<void> _onButtonCreated(ChromeCastController controller) async {
+    _controller = controller;
+    await _controller.addSessionListener();
+  }
+
+  Future<void> _onSessionStarted() async {
+    setState(() => _state = AppState.connected);
+    await _controller.loadMedia(CastSample.csengo);
+  }
+
+  Future<void> _onRequestCompleted() async {
+    final playing = await _controller.isPlaying();
+    setState(() {
+      _state = AppState.mediaLoaded;
+      _playing = playing;
     });
   }
+
+  Future<void> _onRequestFailed(String error) async {
+    setState(() => _state = AppState.error);
+    print(error);
+  }
 }
+
+class _RoundIconButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  _RoundIconButton({@required this.icon, @required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        child: Icon(icon, color: Colors.white), onPressed: onPressed);
+  }
+}
+
+enum AppState { idle, connected, mediaLoaded, error }
